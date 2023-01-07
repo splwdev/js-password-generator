c7a98b0adcda15652819ac5edb492f2cde778a7c
splwebdev <shane_lynch@hotmail.com>
2023-01-07 01:14:06 +0000

    completed first iteration of getpasswordoptions function

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,26 +97,64 @@ let pwdSettings = {
   maxPassword: 64
 }
 
-let pwdLength = 0;
+let pwdLength;
+let pwdOptions = [];
 
 
 // Function to prompt user for password options
 function getPasswordOptions() {
   // Prompting for person to enter in number of characters for password
-  pwdLength = prompt("Please enter a password length between " + pwdSettings.minPassword + " & " + pwdSettings.maxPassword + " characters");
-  
-  // Undertaking while loop until person enters between 10 & 64 chars
-  while (pwdLength < 10 || pwdLength > 64) {
-    alert("Password length must be between " + pwdSettings.minPassword + " & " + pwdSettings.maxPassword);
+  do {
     pwdLength = prompt("Please enter a password length between " + pwdSettings.minPassword + " & " + pwdSettings.maxPassword + " characters");
+
+    // Changed from while loop to do while to minimise code footprint
+    // while (pwdLength < 10 || pwdLength > 64) {
+    // if (isNaN(pwdLength)) {
+    //   alert("Invalid character, please enter a number!!");
+    //   pwdLength = prompt("Please enter a password length between " + pwdSettings.minPassword + " & " + pwdSettings.maxPassword + " characters");
+    // }
+    // else if (pwdLength < 10 || pwdLength > 64) {
+    //   alert("Password length must be between " + pwdSettings.minPassword + " & " + pwdSettings.maxPassword);
+    //   pwdLength = prompt("Please enter a password length between " + pwdSettings.minPassword + " & " + pwdSettings.maxPassword + " characters");
+    // }
+    // else {
+    //   alert("Password length must be between " + pwdSettings.minPassword + " & " + pwdSettings.maxPassword);
+    //   pwdLength = prompt("Please enter a password length between " + pwdSettings.minPassword + " & " + pwdSettings.maxPassword + " characters");
+    // }
+    
+  // }
   }
+  // Testing output if both number and special char added to prompt
+  // alert(isNaN(pwdLength))
+  // Using while loop whilst below conditions are true. Ensures person is continuously prompted until they enter a number between 10 & 64
+  while (pwdLength !== null && (isNaN(pwdLength) === true || pwdLength < 10 || pwdLength > 64 ));
+  
+  // Adding alert and ending function in case user clicks cancel on prompt
+  if (pwdLength === null) {
+    alert("Thank you for looking at the password generator. Goodbye!");
+    return;
+  }
+  
+  
 
   // getting person to confirm password character options
-  let special = confirm("Would you like to include special characters in your password?");
-  let numerical = confirm("Would you like to include numerical characters in your password?");
-  let lowerCase = confirm("Would you like to include lowercase characters in your password?");
-  let upperCase = confirm("Would you like to include uppercase characters in your password?");
+  do {
+    pwdSettings.specialChars = confirm("Would you like to include special characters in your password?");
+    // Testing output
+    // alert(pwdSettings.specialChars);
+    pwdSettings.numericalChars = confirm("Would you like to include numerical characters in your password?");
+    pwdSettings.lowerChars = confirm("Would you like to include lowercase characters in your password?");
+    pwdSettings.upperChars = confirm("Would you like to include uppercase characters in your password?");
+  }
+  // Using while loop whilst below conditions are false to ensure person at least selects one of the options
+  while (pwdSettings.specialChars === false && pwdSettings.numericalChars === false && pwdSettings.lowerChars === false && pwdSettings.upperChars === false);
 
+  // pushing settings to pwdOptions array
+  for (settings in pwdSettings) {
+    if (pwdSettings[settings] === true) {
+      pwdOptions.push(settings); 
+    }
+  }
 }
 
 // Function for getting a random element from an array
@@ -126,7 +164,7 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-
+  getPasswordOptions();
 }
 
 // Get references to the #generate element
